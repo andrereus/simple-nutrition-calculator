46694d9879729b14ed4214e22612293d7dc7d59d
Andre Reus <andrereus@users.noreply.github.com>
2019-03-14 15:46:42 +0100

    Update install instructions

--- src/components/Install.js
+++ src/components/Install.js
@@ -6,7 +6,7 @@ class Install extends Component {
     return (
       <div className="install__wrapper">
         <h2 className="install__title">How to install this Web App</h2>
-        <p>Click "Install Simple Nutrition Calculator ..." or press "Add to homescreen" in the menu of the Google Chrome Browser.</p><br />
+        <p>Click "Install Simple Nutrition Calculator..." or press "Add to homescreen" in the menu of the Google Chrome Browser.</p><br />
         <p>You can install it on any device (Windows, Android, macOS, iOS, Linux).</p><br />
         <div className="install__instruction">
           <img src={Instruction} alt="Install instruction"/>
